b7248600eecfd7915730af717c50b52432760c16
DmitryPoverov <did.ink@mail.ru>
2022-01-03 17:40:36 +0300

    Multithreading Practise 2 was solved

---
+++ src/main/java/by/epam/multithreading/dmDevPractice2/ConcurrentDemo.java
@@ -0,0 +1,45 @@
+package by.epam.multithreading.dmDevPractice2;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 1. Написать программу, бесконечно считывающую пользовательские числа из консоли. Эти числа
+ * представляют собой количество секунд. При каждом вводе числа, должна создаваться задача,
+ * которая засыпает на введённое число секунд и затем выводит "Я спал N секунд". Однако нужно
+ * сделать так, чтобы все задачи выполнялись в одном и том же потоке в порядке ввода. Т.е. в
+ * программе есть 2 потока: главный и поток для выполнения всех задач по очереди. При вводе
+ * отрицательного числа программа должна завершать свою работу.
+ * Второе решение - несколько потоков в пуле. Посчитать кол-во обработанных задач каждым потоком
+ */
+
+public class ConcurrentDemo {
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService threadExecutor = Executors.newSingleThreadExecutor();   // for one thread + main-thread
+        // ExecutorService threadExecutor = Executors.newFixedThreadPool(3);    // for three threads + main-thread
+        ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int seconds = scanner.nextInt();
+            if (seconds < 0) {
+                break;
+            }
+            threadExecutor.submit(() -> {
+                /* (for three threads)
+                Integer counter = threadLocal.get();
+                threadLocal.set(counter== null ? 1 : ++counter);
+                System.out.printf("Stream %s, task: %d\n", Thread.currentThread().getName(), threadLocal.get());*/
+
+                Thread.sleep(seconds * 1000L);
+                System.out.printf("Thread %s slept %d seconds\n", Thread.currentThread().getName(), seconds);
+                return seconds;
+            });
+        }
+        threadExecutor.shutdown();
+        threadExecutor.awaitTermination(10L, TimeUnit.MINUTES);
+    }
+}
---
+++ src/main/java/by/epam/multithreading/dmDevPractice2/OneOrParallelThreadsDemo.java
@@ -0,0 +1,47 @@
+package by.epam.multithreading.dmDevPractice2;
+
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+public class OneOrParallelThreadsDemo {
+    public static void main(String[] args) {
+        int[] values = new int[300_000_000];
+        Random random = new Random();
+        for (int i=0; i< values.length; i++) {
+            values[i] = random.nextInt(300) + 1; // cause nextInt() get random from 0 to 299 (need from 1 to 300)
+        }
+/* Это примитивное решение без StreamAPI
+        long startTime = System.currentTimeMillis();
+        int result = findMaxParallel(values);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Several streams measuring (max=" + result + ") " + (endTime-startTime) + "sec.");
+
+        long startTime = System.currentTimeMillis();
+        int result = findMax(values);
+        long endTime = System.currentTimeMillis();
+        System.out.println("One stream measuring (max=" + result + ") " + (endTime-startTime) + "sec.");*/
+
+        trackTime(() -> findMax(values));
+        trackTime(() -> findMaxParallel(values));
+    }
+
+    private static void trackTime(Supplier<Integer> task) {
+        long startTime = System.currentTimeMillis();
+        int result = task.get();
+        System.out.println(result + (System.currentTimeMillis()-startTime) + " ms.");
+    }
+
+    private static int findMax(int[] values) {
+        return IntStream.of(values)
+                .max()
+                .orElse(Integer.MIN_VALUE);
+    }
+
+    private static int findMaxParallel(int[] values) {
+        return IntStream.of(values)
+                .parallel()
+                .max()
+                .orElse(Integer.MIN_VALUE);
+    }
+}
